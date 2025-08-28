5d3c3db991da08f92d81a62c0c8d0f54032f3210
ashutoshjadhav8 <ashutosh_jadhav@persistent.com>
2025-08-28 02:25:13 -0400

    veriables

--- .github/workflows/runtime.sh
+++ .github/workflows/runtime.sh
@@ -1,4 +1,4 @@
-echo "This is from bash script" | tee /test-summary.txt
+echo "This is from bash script"
 NUM=0
 sdk_versions=0
 REF=0
@@ -10,10 +10,10 @@ RUNTIME_FAIL_AVG=0
 RUNTIME_SKIPPED_TESTCASES=0
 RUNTIME_SKIP_AVG=0
 LIB_BUILD_EXIT_CODE=0
-echo $RUNTIME_PASS_AVG | tee /test-summary.txt
-echo $RUNTIME_SKIP_AVG | tee /test-summary.txt
-echo $RUNTIME_FAIL_AVG | tee /test-summary.txt
-echo Total Test cases Run:$RUNTIME_TOTAL_TESTCASES | tee /test-summary.txt
-echo Test Passed:$RUNTIME_PASSED_TESTCASES | tee /test-summary.txt
-echo Test failed:$RUNTIME_FAILED_TESTCASES | tee /test-summary.txt
-echo Test skipped:$RUNTIME_SKIPPED_TESTCASES | tee /test-summary.txt
+echo $RUNTIME_PASS_AVG
+echo $RUNTIME_SKIP_AVG
+echo $RUNTIME_FAIL_AVG
+echo Total Test cases Run:$RUNTIME_TOTAL_TESTCASES
+echo Test Passed:$RUNTIME_PASSED_TESTCASES
+echo Test failed:$RUNTIME_FAILED_TESTCASES
+echo Test skipped:$RUNTIME_SKIPPED_TESTCASES

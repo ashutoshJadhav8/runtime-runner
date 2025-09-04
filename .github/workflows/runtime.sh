1c9765c2029876e425941eb582894c4d266a67cc
ashutoshjadhav8 <ashutosh_jadhav@persistent.com>
2025-09-04 06:35:39 -0400

    veriables

--- .github/workflows/runtime.sh
+++ .github/workflows/runtime.sh
@@ -1,4 +1,4 @@
-echo "This is from bash script" >> /test-summary.txt
+echo "This is from bash script" >> /script-details
 NUM=0
 sdk_versions=0
 REF=0
@@ -10,10 +10,10 @@ RUNTIME_FAIL_AVG=0
 RUNTIME_SKIPPED_TESTCASES=0
 RUNTIME_SKIP_AVG=0
 LIB_BUILD_EXIT_CODE=0
-echo $RUNTIME_PASS_AVG >> /test-summary.txt
-echo $RUNTIME_SKIP_AVG >> /test-summary.txt
-echo $RUNTIME_FAIL_AVG >> /test-summary.txt
-echo Total Test cases Run:$RUNTIME_TOTAL_TESTCASES >> /test-summary.txt
-echo Test Passed:$RUNTIME_PASSED_TESTCASES >> /test-summary.txt
-echo Test failed:$RUNTIME_FAILED_TESTCASES >> /test-summary.txt
-echo Test skipped:$RUNTIME_SKIPPED_TESTCASES >> /test-summary.txt
+echo $RUNTIME_PASS_AVG >> /script-details
+echo $RUNTIME_SKIP_AVG >> /script-details
+echo $RUNTIME_FAIL_AVG >> /script-details
+echo Total Test cases Run:$RUNTIME_TOTAL_TESTCASES >> /script-details
+echo Test Passed:$RUNTIME_PASSED_TESTCASES >> /script-details
+echo Test failed:$RUNTIME_FAILED_TESTCASES >> /script-details
+echo Test skipped:$RUNTIME_SKIPPED_TESTCASES >> /script-details
